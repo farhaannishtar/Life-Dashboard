79f2fef5df92c6583e5e2f6da210587e8de616e4
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2023-05-24 15:43:24 +0530

    added verification for subscriber endpoint

--- pages/api/fitbitNotifications.ts
+++ pages/api/fitbitNotifications.ts
@@ -1,29 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-type SubscriberParams = {
-  endpointURL: string;
-  format: "JSON";
-  isDefault: boolean;
-  id?: string;
-};
-
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "POST") {
-    const { endpointURL, format, isDefault, id }: SubscriberParams = req.body;
-
-    // Handle the POST request here
-    console.log("Received POST request:", req.body);
-
-    // Process the received parameters
-    console.log("Endpoint URL:", endpointURL);
-    console.log("Format:", format);
-    console.log("Default:", isDefault);
-    console.log("ID:", id);
-
-    // You can perform any necessary actions based on the parameters
-
-    res.status(200).end(); // Send a response indicating successful processing
+  const { verify } = req.query;
+  if (
+    verify ===
+    "a1b59967cbcb7f0c19a0451370bed89a29142a75c6e46d833486b7f7a2bf3b71"
+  ) {
+    res.status(204).end();
   } else {
-    res.status(405).end(); // Method Not Allowed
+    res.status(404).end();
   }
 }
