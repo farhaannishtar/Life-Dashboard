049cbdfee8b16b83ca7909d5bb22328d357c82d4
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2023-05-24 14:41:48 +0530

    added parameters to subscriber endpoint

--- pages/api/fitbitNotifications.ts
+++ pages/api/fitbitNotifications.ts
@@ -1,15 +1,29 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+type SubscriberParams = {
+  endpointURL: string;
+  format: "JSON";
+  isDefault: boolean;
+  id?: string;
+};
+
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
+    const { endpointURL, format, isDefault, id }: SubscriberParams = req.body;
+
     // Handle the POST request here
     console.log("Received POST request:", req.body);
 
-    // You can process the received data and perform any necessary actions
+    // Process the received parameters
+    console.log("Endpoint URL:", endpointURL);
+    console.log("Format:", format);
+    console.log("Default:", isDefault);
+    console.log("ID:", id);
+
+    // You can perform any necessary actions based on the parameters
 
     res.status(200).end(); // Send a response indicating successful processing
   } else {
-    // Handle other HTTP methods if necessary
     res.status(405).end(); // Method Not Allowed
   }
 }
