f5c3a00463d5115cfac83620197630355bd331f4
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2023-08-27 17:44:34 -0400

    moving helper functions into helpers folder

--- helpers/helpers.ts
+++ helpers/helpers.ts
@@ -1,3 +1,5 @@
+import crypto from "crypto";
+
 export const getCurrentDate = (): string => {
   const now = new Date();
   const year = now.getFullYear().toString();
@@ -7,15 +9,17 @@ export const getCurrentDate = (): string => {
   return currentDate;
 };
 
-export const getPreviousDate = (): string => {
-  const now = new Date();
-  now.setDate(now.getDate() - 1);
-  const year = now.getFullYear().toString();
-  const month = (now.getMonth() + 1).toString().padStart(2, "0");
-  const day = now.getDate().toString().padStart(2, "0");
-  const previousDate = `${year}-${month}-${day}`;
-  return previousDate;
-};
+export function base64URLEncode(str: any) {
+  return str
+    .toString("base64")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=/g, "");
+}
+
+export function sha256(buffer: any) {
+  return crypto.createHash("sha256").update(buffer).digest();
+}
 
 export const calculateSleepScorePercentageChange = (
   oldScore: number,
