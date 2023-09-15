a76e19676105ac6daf0553f162fe1d53204cc8f7
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2023-09-15 07:55:46 -0400

    fix: touch up styling in HabitStreakCard component

--- components/HabitStreakCard.tsx
+++ components/HabitStreakCard.tsx
@@ -13,12 +13,12 @@ function HabitStreakCard({ streak, borderColor, textColor, bgColor }: HabitStrea
       <div className='pt-6 pl-6 text-26 leading-8 font-bold m-0 w-full'> 
         🔥
       </div>
-      <div className={`py-5 px-6 font-black text-64 leading-10 inline-block align-middle`}
+      <div className={`py-4 px-6 font-black text-64 leading-10 inline-block align-middle`}
         style={{ color: textColor }}
       >
         {streak} <span className="text-lg -ml-3">day</span>
       </div>
-      <div className={`w-full text-center font-black text-lg leading-5 pb-3.5`}
+      <div className={`w-full text-center font-black text-lg leading-5 pb-5`}
         style={{ color: textColor }}
       >
        Streak
