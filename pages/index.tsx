67fa99d1fc596214aade1f2ba3937ec460ae49d4
Farhaan Nishtar <farhaanmnishtar99@gmail.com>
2023-09-14 21:57:07 -0400

    add connecting line connecting HabitWeekCalendar and HabitStreakCard components

--- pages/index.tsx
+++ pages/index.tsx
@@ -171,9 +171,14 @@ export default function Home({ fitbitData }: InferGetServerSidePropsType<typeof 
       >
         Daily Vows
       </div>
-      <div className='w-full flex mt-5 gap-x-6'>
+      <div className='w-full flex mt-5 items-center'>
         <div className="flex-grow-2 flex-shrink">
-          <HabitWeekCalendar /> 
+          <HabitWeekCalendar />
+        </div>
+        <div className="flex-none">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="2" viewBox="0 0 20 2" fill="none">
+            <path d="M0 1L20 1" stroke="#B6C8DA" stroke-dasharray="1 1"/>
+          </svg>
         </div>
         <div className="flex-grow flex-shrink">
           <HabitStreakCard 
@@ -186,8 +191,9 @@ export default function Home({ fitbitData }: InferGetServerSidePropsType<typeof 
       </div>
 
 
-      <div className='flex mt-32'>  
-      </div>
+
+
+    <div className='flex mt-32'></div>
     </div>
   )
 }
